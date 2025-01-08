8cf27c2636dfefd88edc29e6c403c97d9e9b0151
mrsharma2404 <mrsharma2404@egmail.com>
2025-01-09 00:03:54 +0530

    added color prop in icon

--- src/app/icons/IssueIcon.tsx
+++ src/app/icons/IssueIcon.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const IssueIcon = () => {
+const IssueIcon = ({ color }: { color?: string }) => {
   return (
     <svg
       viewBox="0 0 16 16"
@@ -8,7 +8,7 @@ const IssueIcon = () => {
       width="16"
       height="16"
       aria-hidden="true"
-      fill="green"
+      fill={color || "green"}
     >
       <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z"></path>
       <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0ZM1.5 8a6.5 6.5 0 1 0 13 0 6.5 6.5 0 0 0-13 0Z"></path>
